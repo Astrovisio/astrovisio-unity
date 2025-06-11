2ed47a810d37cb4aa56d1795aead50a7c35871f5
c.mabilia <cosimo.mabilia@gmail.com>
2025-06-11 16:27:43 +0200

    Refactored KDTreeComponent

--- Assets/_Astrovisio/Scripts/CatalogData/KDTreeComponent.cs
+++ Assets/_Astrovisio/Scripts/CatalogData/KDTreeComponent.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using System.Threading.Tasks;
-using Unity.XR.CoreUtils;
 
 public class KDTreeComponent : MonoBehaviour
 {
+    [Header("Settings")]
+    public bool realtime = true;
+
     [Header("Transforms")]
     public Transform pointCloudTransform;
     public Transform controllerTransform;
@@ -12,16 +14,18 @@ public class KDTreeComponent : MonoBehaviour
     public Vector2 xRange = new Vector2(-10f, 10f);
     public Vector2 yRange = new Vector2(-10f, 10f);
     public Vector2 zRange = new Vector2(-10f, 10f);
-    public Vector2 normalizedRange = new Vector2(-1f, 1f);
+    public Vector2 xTargetRange = new Vector2(-10f, 10f);
+    public Vector2 yTargetRange = new Vector2(-10f, 10f);
+    public Vector2 zTargetRange = new Vector2(-10f, 10f);
 
 
-    private float gizmoRadius = 0.02f;
     private KDTreeManager manager;
     private float[][] data;
-    private (int, float)? nearest;
+    private (int, float)? nearest; // (Index, SquaredDistance)
     private bool running = false;
 
     [Header("Debug Sphere (in game)")]
+    public bool debugSphereEnabled = true;
     public Color sphereColor = Color.red;
     public float sphereRadius = 0.05f;
     private GameObject debugSphere;
@@ -40,6 +44,7 @@ public class KDTreeComponent : MonoBehaviour
         if (debugSphere == null)
         {
             debugSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            debugSphere.SetActive(false);
             debugSphere.name = "DebugNearestPointSphere";
             debugSphere.transform.localScale = Vector3.one * sphereRadius * 2f; // diametro = raggio*2
             var rend = debugSphere.GetComponent<Renderer>();
@@ -51,6 +56,49 @@ public class KDTreeComponent : MonoBehaviour
         }
     }
 
+    public (int, float)? getLastNearest()
+    {
+        return nearest;
+    }
+
+    public Vector3 getNearestWorldSpaceCoordinates(int? index)
+    {
+        int lastNearestIndex = index != null ? (int)index : nearest.Value.Item1;
+        Vector3 pointOriginal = new Vector3(
+            data[0][lastNearestIndex],
+            data[1][lastNearestIndex],
+            data[2][lastNearestIndex]
+        );
+
+        pointOriginal.x = RemapInverse(pointOriginal.x, xRange, xTargetRange);
+        pointOriginal.y = RemapInverse(pointOriginal.y, yRange, yTargetRange);
+        pointOriginal.z = RemapInverse(pointOriginal.z, zRange, zTargetRange);
+
+        Vector3 worldPos = pointCloudTransform.TransformPoint(pointOriginal);
+        return worldPos;
+    }
+
+    public async Task<(int, float)> ComputeNearestPoint(Vector3 point)
+    {
+        Vector3 controllerLocal = pointCloudTransform.InverseTransformPoint(point);
+
+        float x = RemapInverse(controllerLocal.x, xTargetRange, xRange);
+        float y = RemapInverse(controllerLocal.y, yTargetRange, yRange);
+        float z = RemapInverse(controllerLocal.z, zTargetRange, zRange);
+        Vector3 queryPoint = new Vector3(x, y, z);
+
+        (int, float) nearest = await Task.Run(() => manager.FindNearest(queryPoint));
+
+        return nearest;
+    }
+
+    [ContextMenu("ComputeNearestPoint")]
+    public async Task<(int, float)?> ComputeNearestPoint()
+    {
+        nearest = await ComputeNearestPoint(controllerTransform.position);
+        return nearest;
+    }
+
     private async void Update()
     {
         if (manager == null || running || controllerTransform == null || pointCloudTransform == null)
@@ -58,64 +106,38 @@ public class KDTreeComponent : MonoBehaviour
             return;
         }
 
-        running = true;
-        Vector3 controllerWorld = controllerTransform.position;
-        Vector3 controllerLocal = pointCloudTransform.InverseTransformPoint(controllerWorld);
 
-        float x = RemapInverse(controllerLocal.x, normalizedRange, xRange);
-        float y = RemapInverse(controllerLocal.y, normalizedRange, yRange);
-        float z = RemapInverse(controllerLocal.z, normalizedRange, zRange);
-        Vector3 queryPoint = new Vector3(x, y, z);
-
-        nearest = await Task.Run(() => manager.FindNearest(queryPoint));
-
-        var lastNearestIndex = nearest.Value.Item1;
-        // Sposta la sfera debug nel mondo
-        if (debugSphere != null && lastNearestIndex >= 0)
+        if (realtime)
         {
-            Vector3 pointOriginal = new Vector3(
-                data[0][lastNearestIndex],
-                data[1][lastNearestIndex],
-                data[2][lastNearestIndex]
-            );
-
-            pointOriginal.x = RemapInverse(pointOriginal.x, xRange, normalizedRange);
-            pointOriginal.y = RemapInverse(pointOriginal.y, yRange, normalizedRange);
-            pointOriginal.z = RemapInverse(pointOriginal.z, zRange, normalizedRange);
-
-            Vector3 worldPos = pointCloudTransform.TransformPoint(pointOriginal);
-            debugSphere.transform.position = worldPos;
+            running = true;
+            nearest = await ComputeNearestPoint(controllerTransform.position);
+            running = false;
         }
 
-        running = false;
+        // Sposta la sfera debug nel mondo
+        if (nearest != null && debugSphereEnabled && debugSphere != null)
+        {
+            if (!debugSphere.activeSelf)
+            {
+                debugSphere.SetActive(true);
+            }
+            debugSphere.transform.position = getNearestWorldSpaceCoordinates(nearest.Value.Item1);
+        }
+        else if (!debugSphereEnabled)
+        {
+            if (debugSphere.activeSelf)
+            {
+                debugSphere.SetActive(false);
+            }
+        }
+
+
     }
 
-    float RemapInverse(float val, Vector2 from, Vector2 to)
+    private float RemapInverse(float val, Vector2 from, Vector2 to)
     {
         float t = Mathf.InverseLerp(from.x, from.y, val);
         return Mathf.Lerp(to.x, to.y, t);
     }
 
-    private void OnDrawGizmos()
-    {
-        if (!Application.isPlaying || nearest == null) return;
-        var lastNearestIndex = nearest.Value.Item1;
-
-        // Ottieni la posizione originale del punto nel sistema dati
-        Vector3 pointOriginal = new Vector3(
-            data[0][lastNearestIndex],
-            data[1][lastNearestIndex],
-            data[2][lastNearestIndex]
-        );
-
-        pointOriginal.x = RemapInverse(pointOriginal.x, xRange, normalizedRange);
-        pointOriginal.y = RemapInverse(pointOriginal.y, yRange, normalizedRange);
-        pointOriginal.z = RemapInverse(pointOriginal.z, zRange, normalizedRange);
-
-        // Trasformalo nello spazio locale del pointCloudTransform, poi in mondo
-        Vector3 worldPos = pointCloudTransform.TransformPoint(pointOriginal);
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(worldPos, gizmoRadius);
-    }
 }
